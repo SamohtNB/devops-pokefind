cc7a9ad7a0304f122650f814153d2f2755f41302
SamohtNB <60231047+SamohtNB@users.noreply.github.com>
2025-07-11 10:13:57 +0200

    Enhance Docker configuration: add environment variables for port settings and update entrypoint script for better clarity and functionality

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# entrypoint.sh
 
-# Valeur par défaut si PORT non défini
+# Par défaut, port interne nginx
 PORT="${PORT:-80}"
+# Port sur lequel on accède depuis l'hôte
+HOST_PORT="${HOST_PORT:-3000}"
 
-echo "🚀 Starting server: listening on port $PORT"
+# On affiche un lien cliquable vers localhost
+echo "🚀 Votre application est disponible ici → http://localhost:${HOST_PORT}"
 
-# Lancement de nginx, en injectant le port dans la conf si besoin
-# (pour nginx.conf custom, on suppose que listen $PORT; y est déjà)
+# On démarre Nginx
 exec nginx -g "daemon off;"
